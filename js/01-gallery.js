4ed5653362157f5de0eb5158d56c0307b8e5ed3c
Sergio <114320133+sergioing1000@users.noreply.github.com>
2023-05-06 00:47:23 -0500

    modified:   01-gallery.html 	modified:   js/01-gallery.js 	new file:   node_modules/.package-lock.json 	new file:   node_modules/basiclightbox/CHANGELOG.md 	new file:   node_modules/basiclightbox/LICENSE 	new file:   node_modules/basiclightbox/README.md 	new file:   node_modules/basiclightbox/dist/basicLightbox.min.css 	new file:   node_modules/basiclightbox/dist/basicLightbox.min.js 	new file:   node_modules/basiclightbox/package.json 	new file:   node_modules/basiclightbox/src/.DS_Store 	new file:   node_modules/basiclightbox/src/scripts/main.js 	new file:   node_modules/basiclightbox/src/styles/main.scss 	new file:   package-lock.json 	new file:   package.json

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,10 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 
+// ⬇️⬇️⬇️Following lines include a link in the head for the styles files CSS. According the recomendation of basicLightbox repository.
+
+const upperHead = document.querySelector("head");
+const stylesFile = document.createElement("link");
+stylesFile.rel = "stylesheet";
+stylesFile.href = "/node_modules/basiclightbox/dist/basicLightbox.min.css";
+upperHead.append(stylesFile);
+
+// ⬇️⬇️⬇️Following lines include a js script at the end of the body. According the recomendation of basicLightbox repository.
+
+const bottomBody = document.querySelector("body");
+const jsLink = document.createElement("script");
+jsLink.src = "/node_modules/basiclightbox/dist/basicLightbox.min.js";
+bottomBody.append(jsLink);
+
+// ⬇️⬇️⬇️ Following lines create the gallery using a for each loop in the imported array coming from gallery-items.js file' On top of that these lines add classes to include styles alreedy defined in the styles.css file.
+
 const uList = document.querySelector("ul.gallery");
 
-
-galleryItems.forEach(element => {
+galleryItems.forEach(array_element => {
   
   const liItem = document.createElement("li");
   uList.append(liItem);
@@ -13,13 +29,30 @@ galleryItems.forEach(element => {
 
   let newAnchor = document.createElement("a");
   liItem.append(newAnchor);
-  newAnchor.href = element.original;
+  newAnchor.href = array_element.original;
   newAnchor.classList.add("gallery__link");
 
   let newImg = document.createElement("img");
-  newImg.src = element.preview;
-  newImg.alt = element.description;
+  newImg.src = array_element.preview;
+  newImg.alt = array_element.description;
   newAnchor.append(newImg);
   newImg.classList.add("gallery__image");
 
 });
+
+// ⬇️⬇️⬇️ Following lines will include basiclightbox methods.
+
+const anchor = document.querySelector(".gallery__image");
+
+console.log(anchor);
+
+anchor.addEventListener("click", (event) => {
+
+  event.preventDefault();
+  console.log("OJO dio click");
+
+})
+
+
+
+
