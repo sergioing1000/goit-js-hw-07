6dc1636d0ca15b4ed3788f3a7d79139f7aba357c
Sergio <114320133+sergioing1000@users.noreply.github.com>
2023-05-07 14:21:10 -0500

    modified:   js/02-lightbox.js 	modified:   node_modules/.package-lock.json 	new file:   node_modules/simplelightbox/.babelrc 	new file:   node_modules/simplelightbox/.github/FUNDING.yml 	new file:   node_modules/simplelightbox/LICENSE 	new file:   node_modules/simplelightbox/README.md 	new file:   node_modules/simplelightbox/bower.json 	new file:   node_modules/simplelightbox/demo/demo.css 	new file:   node_modules/simplelightbox/demo/img/bg.jpg 	new file:   node_modules/simplelightbox/demo/img/full/01.jpg 	new file:   node_modules/simplelightbox/demo/img/full/02.jpg 	new file:   node_modules/simplelightbox/demo/img/full/03.jpg 	new file:   node_modules/simplelightbox/demo/img/full/04.jpg 	new file:   node_modules/simplelightbox/demo/img/full/05.jpg 	new file:   node_modules/simplelightbox/demo/img/full/06.jpg 	new file:   node_modules/simplelightbox/demo/img/mouse.svg 	new file:   node_modules/simplelightbox/demo/img/thumb/01.jpg 	new file:   node_modules/simplelightbox/demo/img/thumb/02.jpg 	new file:   node_modules/simplelightbox/demo/img/thumb/03.jpg 	new file:   node_modules/simplelightbox/demo/img/thumb/04.jpg 	new file:   node_modules/simplelightbox/demo/img/thumb/05.jpg 	new file:   node_modules/simplelightbox/demo/img/thumb/06.jpg 	new file:   node_modules/simplelightbox/demo/index.html 	new file:   node_modules/simplelightbox/demo/onlyImages.html 	new file:   node_modules/simplelightbox/dist/simple-lightbox.css 	new file:   node_modules/simplelightbox/dist/simple-lightbox.esm.js 	new file:   node_modules/simplelightbox/dist/simple-lightbox.jquery.js 	new file:   node_modules/simplelightbox/dist/simple-lightbox.jquery.min.js 	new file:   node_modules/simplelightbox/dist/simple-lightbox.js 	new file:   node_modules/simplelightbox/dist/simple-lightbox.legacy.js 	new file:   node_modules/simplelightbox/dist/simple-lightbox.legacy.min.js 	new file:   node_modules/simplelightbox/dist/simple-lightbox.min.css 	new file:   node_modules/simplelightbox/dist/simple-lightbox.min.js 	new file:   node_modules/simplelightbox/dist/simple-lightbox.modules.js 	new file:   node_modules/simplelightbox/gulpfile.js 	new file:   node_modules/simplelightbox/package.json 	new file:   node_modules/simplelightbox/src/jquery-plugin-wrap.js 	new file:   node_modules/simplelightbox/src/legacy.js 	new file:   node_modules/simplelightbox/src/license-notice.txt 	new file:   node_modules/simplelightbox/src/simple-lightbox.js 	new file:   node_modules/simplelightbox/src/simple-lightbox.scss 	modified:   package-lock.json 	modified:   package.json

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,103 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let i;
+
+// ⬇️⬇️⬇️Following lines include a link in the head for the styles files CSS. According the recomendation of simpleLightbox repository.
+
+const upperHead = document.querySelector("head");
+const stylesFile = document.createElement("link");
+stylesFile.rel = "stylesheet";
+
+stylesFile.href = "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.13.0/simple-lightbox.css";
+upperHead.append(stylesFile);
+
+// ⬇️⬇️⬇️Following lines include a js script at the end of the body. According the recomendation of simpleLightbox repository.
+
+const bottomBody = document.querySelector("body");
+const jsLink = document.createElement("script");
+jsLink.src = "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.13.0/simple-lightbox.min.js";
+bottomBody.append(jsLink);
+
+// ⬇️⬇️⬇️ Following lines create the gallery using a for each loop in the imported array coming from gallery-items.js file' On top of that these lines add classes to include styles alreedy defined in the styles.css file.
+
+const uList = document.querySelector("ul.gallery");
+
+galleryItems.forEach(array_element => {
+  
+  const liItem = document.createElement("li");
+  uList.append(liItem);
+  liItem.classList.add("gallery__item");
+
+  let newAnchor = document.createElement("a");
+  liItem.append(newAnchor);
+  newAnchor.href = array_element.original;
+  newAnchor.classList.add("gallery__link");
+
+  let newImg = document.createElement("img");
+  newImg.src = array_element.preview;
+  newImg.alt = array_element.description;
+  newAnchor.append(newImg);
+  newImg.classList.add("gallery__image");
+
+});
+
+
+// ⬇️⬇️⬇️ Following lines will include basiclightbox methods.
+
+// const anchors = document.querySelectorAll(".gallery__link");
+
+
+// anchors.forEach((elemento,  i=0) => {
+  
+//   elemento.addEventListener("click", function (event) {
+//     event.preventDefault();
+    
+//     // console.log(`El valor de i es: ${i} y el link de la imagen es ${galleryItems[i - 1].original}`);
+    
+//     let imageOnModal=simpleLightbox.create(
+//       `
+// 	  	<img width="1400" height="900" src=${galleryItems[i - 1].original}>
+//   	  `,
+//       {
+//         closable:true
+//       }
+//       );
+      
+//       imageOnModal.show();
+      
+//       document.addEventListener('keydown', function(event) {
+//         if (event.key === 'Escape') {
+//           // Your event handling code here
+          
+//           imageOnModal.close();
+//           beep(500, 440);
+          
+//         }
+//       });
+      
+//     });
+//     i++;
+//   });
+  
+  var lightbox = new SimpleLightbox('.gallery a');
+  
+  function beep(duration, frequency) {
+    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    
+  oscillator.type = 'square';
+  oscillator.frequency.value = frequency;
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
+  
+  oscillator.start();
+  gainNode.gain.setValueAtTime(1, audioContext.currentTime);
+  gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + duration / 1000);
+
+  setTimeout(function() {
+    oscillator.stop();
+    audioContext.close();
+  }, duration);
+}
